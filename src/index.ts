73366a5236256b2c4ecffe75aae8d5974cff1318
Kobiyami <fournierg01@gmail.com>
2026-06-27 17:06:58 +0200

    feat: public API entry point via index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { Tournament } from './tournament'
+export { exportTRF } from './trf'
+export { generateRound } from './generateRound'
+export { applyRoundResults } from './applyRound'
+export { initStanding, buildStandingsFromHistory } from './standings'
+export { sortByRanking, groupByScore } from './ranking'
+export type {
+  Player,
+  PlayerStanding,
+  GameResult,
+  GameResultInput,
+  Pairing,
+  RoundPairings,
+  TournamentInfo,
+  Color,
+  GameOutcome,
+} from './types'
